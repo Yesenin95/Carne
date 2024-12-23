1149d928db222b06fbcdbc84616faccf6811e613
Николай <nikolatrofik@gmail.com>
2024-12-23 20:14:44 +0300

    Мелкие правки

--- app/ui/components/card/card.tsx
+++ app/ui/components/card/card.tsx
@@ -1,8 +1,9 @@
+import Image from "next/image";
 import styles from "./page.module.css";
 export default function Card() {
    return <>
       <div className={styles.div}>
-         <img src="" alt="" />
+         <Image src="" alt="" width={100} height={100}/>
          <p>Пельмени</p>
          <p>1000р <span>1кг</span></p>
          <button>Подробнее</button>
